ec2b364fac79209adb2ecc7a976fa57581bdd5f3
AhmadHammad21 <ahmedhammad8765@gmail.com>
2025-07-12 21:26:07 +0300

    logs

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -29,7 +29,12 @@ def load_best_model_local():
 
 
 def load_best_model():
-    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    # Set tracking URI based on environment
+    if os.environ.get("AWS_LAMBDA_FUNCTION_NAME"):
+        # In Lambda, use a dummy tracking URI for inference
+        mlflow.set_tracking_uri("file:///tmp/mlflow")
+    else:
+        mlflow.set_tracking_uri("sqlite:///mlflow.db")
     
     # mlflow.set_tracking_uri("http://mlflow:5000")
 
