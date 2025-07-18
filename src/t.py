e68b217c342c24b7dee417894839130cb898dfad
AhmadHammad21 <ahmedhammad8765@gmail.com>
2025-07-18 21:27:17 +0300

    remove training

--- src/t.py
+++ src/t.py
@@ -1,16 +1,18 @@
 import requests
 
-url = 'https://iy7gped0wa.execute-api.us-east-1.amazonaws.com/dev/taxi-prediction-dev'
+url = 'https://kcqpx7zv7r4feu2j2by2ntiaqe0szpet.lambda-url.us-east-1.on.aws/api/v1/'
 
+response = requests.get(url)
+print(response)
 # Example data to send (change according to your API)
-payload = {
-    "feature1": 1.23,
-    "feature2": 4.56
-}
+# payload = {
+#     "DOLocationID": "x",
+#     "PULocationID": "y"
+# }
 
 
-response = requests.post(url, json=payload)
+# response = requests.post(url, json=payload)
 
-# print the response
-print(response.status_code)
-print(response.json())
+# # print the response
+# print(response.status_code)
+# print(response.json())
