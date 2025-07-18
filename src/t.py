370c18456ace427e45bda37919494927813242fe
AhmadHammad21 <ahmedhammad8765@gmail.com>
2025-07-19 00:24:53 +0300

    URL

--- src/t.py
+++ src/t.py
@@ -1,18 +1,19 @@
 import requests
 
-url = 'https://kcqpx7zv7r4feu2j2by2ntiaqe0szpet.lambda-url.us-east-1.on.aws/api/v1/'
+url = 'https://kcqpx7zv7r4feu2j2by2ntiaqe0szpet.lambda-url.us-east-1.on.aws/api/v1/predict'
 
-response = requests.get(url)
-print(response)
+# response = requests.get(url)
+# print(response)
 # Example data to send (change according to your API)
-# payload = {
-#     "DOLocationID": "x",
-#     "PULocationID": "y"
-# }
+payload = {
+    "DOLocationID": "x",
+    "PULocationID": "y",
+    "trip_distance": 5
+}
 
 
-# response = requests.post(url, json=payload)
+response = requests.post(url, json=payload)
 
-# # print the response
-# print(response.status_code)
-# print(response.json())
+# print the response
+print(response.status_code)
+print(response.json())
